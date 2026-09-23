51c9552cd5e0e8cef85732f357ccceb983f9a9bd
Tim Lehming <timlehming@aol.com>
2026-09-23 17:03:36 +0200

    done

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,24 @@
 // Iteration 2 | Functions
 //
 
+function calcMultiplication(a: number, b: number): number {
+  return a * b;
+}
 
+function isEven(n: number): boolean {
+  return n % 2 === 0;
+}
+
+function calcArrayAverage(numbersArr: number[]): number | string {
+  if (numbersArr.length === 0) {
+    return "Please provide an array with at least one element";
+  }
+
+  const sumOfNumbers = numbersArr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
+
+  const avg = sumOfNumbers / numbersArr.length;
+
+  return avg;
+}
